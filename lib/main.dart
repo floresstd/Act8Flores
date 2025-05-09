8675d944162b624dc59a17adb9590051c6f28ea2
JESUS_DANIEL_FLORES_RODRIGUEZ <a.22308051281186@cbtis128.edu.mx>
2025-05-09 19:04:41 +0000

    fsdgretgregre

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,8 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-void main() => runApp(
-  MaterialApp(
-    debugShowCheckedModeBanner: false,
-    home: HomePage()
-  )
-);
+void main() =>
+    runApp(MaterialApp(debugShowCheckedModeBanner: false, home: HomePage()));
 
 class HomePage extends StatefulWidget {
   @override
@@ -14,28 +10,27 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-
   final List<Map<String, dynamic>> _listItem = [
     {"image": 'assets/images/two.jpg', "isSaved": false},
-    {"image": 'assets/images/three.jpg', "isSaved": false},
-    {"image": 'assets/images/four.jpg', "isSaved": true},
-    {"image": 'assets/images/five.jpg', "isSaved": true},
-    {"image": 'assets/images/one.jpg', "isSaved": false},
+    {"image": 'assets/images/three.png', "isSaved": false},
+    {"image": 'assets/images/four.png', "isSaved": true},
+    {"image": 'assets/images/five.png', "isSaved": true},
+    {"image": 'assets/images/one.png', "isSaved": false},
     {"image": 'assets/images/two.jpg', "isSaved": false},
-    {"image": 'assets/images/three.jpg', "isSaved": false},
-    {"image": 'assets/images/four.jpg', "isSaved": false},
-    {"image": 'assets/images/five.jpg', "isSaved": false},
+    {"image": 'assets/images/three.png', "isSaved": false},
+    {"image": 'assets/images/four.png', "isSaved": false},
+    {"image": 'assets/images/five.png', "isSaved": false},
   ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.grey[600],
+      backgroundColor: Colors.white,
       appBar: AppBar(
         backgroundColor: Colors.transparent,
         elevation: 0,
         leading: Icon(Icons.menu),
-        title: Text("Home"),
+        title: Text("Postres"),
         actions: <Widget>[
           Padding(
             padding: EdgeInsets.all(10.0),
@@ -43,9 +38,7 @@ class _HomePageState extends State<HomePage> {
               width: 36,
               height: 30,
               decoration: BoxDecoration(
-                color: Colors.grey[800],
-                borderRadius: BorderRadius.circular(10)
-              ),
+                  color: Colors.white, borderRadius: BorderRadius.circular(10)),
               child: Center(child: Text("0")),
             ),
           )
@@ -60,101 +53,117 @@ class _HomePageState extends State<HomePage> {
                 width: double.infinity,
                 height: 250,
                 decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(20),
-                  image: DecorationImage(
-                    image: AssetImage('assets/images/one.jpg'),
-                    fit: BoxFit.cover
-                  )
-                ),
+                    borderRadius: BorderRadius.circular(20),
+                    image: DecorationImage(
+                        image: AssetImage('assets/images/one.png'),
+                        fit: BoxFit.cover)),
                 child: Container(
                   decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(20),
-                    gradient: LinearGradient(
-                      begin: Alignment.bottomRight,
-                      colors: [
+                      borderRadius: BorderRadius.circular(20),
+                      gradient:
+                          LinearGradient(begin: Alignment.bottomRight, colors: [
+                        Colors.black.withOpacity(.5),
                         Colors.black.withOpacity(.4),
-                        Colors.black.withOpacity(.2),
-                      ]
-                    )
-                  ),
+                      ])),
                   child: Column(
                     mainAxisAlignment: MainAxisAlignment.end,
                     children: <Widget>[
-                      Text("Lifestyle Sale", style: TextStyle(color: Colors.white, fontSize: 35, fontWeight: FontWeight.bold),),
-                      SizedBox(height: 30,),
+                      Text(
+                        "Ofertas increibles",
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 35,
+                            fontWeight: FontWeight.bold),
+                      ),
+                      SizedBox(
+                        height: 30,
+                      ),
                       Container(
                         height: 50,
                         margin: EdgeInsets.symmetric(horizontal: 40),
                         decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(10),
-                          color: Colors.white
-                        ),
-                        child: Center(child: Text("Shop Now", style: TextStyle(color: Colors.grey[900], fontWeight: FontWeight.bold),)),
+                            borderRadius: BorderRadius.circular(10),
+                            color: Colors.white),
+                        child: Center(
+                            child: Text(
+                          "Comprar ahora",
+                          style: TextStyle(
+                              color: Colors.grey[900],
+                              fontWeight: FontWeight.bold),
+                        )),
                       ),
-                      SizedBox(height: 30,),
+                      SizedBox(
+                        height: 30,
+                      ),
                     ],
                   ),
                 ),
               ),
-              SizedBox(height: 20,),
+              SizedBox(
+                height: 20,
+              ),
               Expanded(
-                child: GridView.count(
-                  crossAxisCount: 2,
-                  crossAxisSpacing: 10,
-                  mainAxisSpacing: 10,
-                  children: _listItem.map((item) => Card(
-                    color: Colors.transparent,
-                    elevation: 0,
-                    child: Container(
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(20),
-                        image: DecorationImage(
-                          image: AssetImage(item["image"]),
-                          fit: BoxFit.cover
-                        )
-                      ),
-                      child: Transform.translate(
-                        offset: Offset(55, -58),
-                        child: Container(
-                          width: 30,
-                          // height: 30,
-                          clipBehavior: Clip.hardEdge,
-                          decoration: BoxDecoration(),
-                          child: Center(
-                            child: MaterialButton(
-                              onPressed: () {
-                                setState(() {
-                                  item["isSaved"] = !item["isSaved"];
-                                  
-                                });
-                              },
-                              color: Colors.white,
-                              height: 35,
-                              minWidth: 40,
-                              padding: EdgeInsets.all(0),
-                              shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(8)
+                  child: GridView.count(
+                crossAxisCount: 2,
+                crossAxisSpacing: 10,
+                mainAxisSpacing: 10,
+                children: _listItem
+                    .map((item) => Card(
+                          color: Colors.transparent,
+                          elevation: 0,
+                          child: Container(
+                            decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(20),
+                                image: DecorationImage(
+                                    image: AssetImage(item["image"]),
+                                    fit: BoxFit.cover)),
+                            child: Transform.translate(
+                              offset: Offset(55, -58),
+                              child: Container(
+                                width: 30,
+                                // height: 30,
+                                clipBehavior: Clip.hardEdge,
+                                decoration: BoxDecoration(),
+                                child: Center(
+                                  child: MaterialButton(
+                                    onPressed: () {
+                                      setState(() {
+                                        item["isSaved"] = !item["isSaved"];
+                                      });
+                                    },
+                                    color: Colors.white,
+                                    height: 35,
+                                    minWidth: 40,
+                                    padding: EdgeInsets.all(0),
+                                    shape: RoundedRectangleBorder(
+                                        borderRadius: BorderRadius.circular(8)),
+                                    child: Icon(
+                                        item["isSaved"]
+                                            ? Icons.bookmark
+                                            : Icons.bookmark_border,
+                                        size: 22,
+                                        color: item["isSaved"]
+                                            ? Colors.yellow[700]
+                                            : Colors.black),
+                                  ),
+                                ),
                               ),
-                              child: Icon(item["isSaved"] ? Icons.bookmark : Icons.bookmark_border, size: 22, color: item["isSaved"] ? Colors.yellow[700] : Colors.black),
+                              // child: InkWell(
+                              //   onLongPress: () {},
+                              //   child: Container(
+                              //     margin: EdgeInsets.symmetric(horizontal:70, vertical: 71),
+                              //     decoration: BoxDecoration(
+                              //       borderRadius: BorderRadius.circular(8),
+                              //       color: Colors.white
+                              //     ),
+                              //     child: Icon(Icons.bookmark_border, size: 22,),
+                              //   ),
+                              // ),
                             ),
                           ),
-                        ),
-                        // child: InkWell(
-                        //   onLongPress: () {},
-                        //   child: Container(
-                        //     margin: EdgeInsets.symmetric(horizontal:70, vertical: 71),
-                        //     decoration: BoxDecoration(
-                        //       borderRadius: BorderRadius.circular(8),
-                        //       color: Colors.white
-                        //     ),
-                        //     child: Icon(Icons.bookmark_border, size: 22,),
-                        //   ),
-                        // ),
-                      ),
-                    ),
-                  )).toList(),
-                )
-              )
+                        ))
+                    .toList(),
+              ))
             ],
           ),
         ),
